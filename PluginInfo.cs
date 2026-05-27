cb1e77ab8ee5f36d93d4c47ab7def20062601ec4
DeezVr <normajperez1030@gmail.com>
2026-05-27 17:37:52 -0400

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.deez.gorillatag.gorillamedia";
         public const string Name = "Deez's Gorilla Media";
-        public const string Version = "1.0.2";
+        public const string Version = "1.0.3";
     }
 }
